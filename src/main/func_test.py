ef51787965d8c38a9f6f2061959d3f059a448406
nuoc <nuoc@kth.se>
2020-07-31 19:22:43 +0200

    testing

--- src/main/func_test.py
+++ src/main/func_test.py
@@ -1,13 +1,14 @@
 import DataManager as manager
 import DataPuller as puller
 import Predictors as pr
+import plotly.express as ex
 
 # data = manager.pull_prepare_save("1997-01-01", "2020-07-19", "wk")
 # pr.train_eval_save("MLP", data, filename="test")
 
 # PULL APPEND SAVE
 # data = manager.load("1997-01-01-2020-07-19:1d")
-# data = manager.pull_append_prepare_save("2020-07-20", "2020-07-23", "1d", data)
+# data = manager.pull_append_prepare_save("2020-07-20", "2020-07-31", "1d", data)
 # data = manager.load("1997-01-01-2020-07-19:1d")
 # data = manager.pull_prepare_save("1997-01-01", "2020-07-19", "1mo")
 #---------------------------------------------------------
@@ -33,14 +34,29 @@ import Predictors as pr
 # pr.load_eval_save("RF", data, "RF_2")
 #----------------------------------------------------------
 
-data = manager.load("2020-07-20-2020-07-23:1d")
+# SIMPLE EVAL
+data = manager.load("2020-07-20-2020-07-31:1d")
 mlp = pr.mlp_load("MLP_2")
 rf = pr.random_forests_load("RF_2")
 bayes = pr.bayes_load("BAYES_2")
 pre1, ae, mae, std = pr.evaluate(mlp, data[-5:], "inter-diff", True)
 pre2, ae, mae, std = pr.evaluate(rf, data[-5:], "inter-diff", True)
 pre3, ae, mae, std = pr.evaluate(bayes, data[-5:], "inter-diff", True)
+
+high = (data["high"] - data["open"])/data["open"] * 100
+low = (data["low"] - data["open"])/data["open"] * 100
+
+print("ACTUAL:")
+print(data["inter-diff"][-5:])
+print("HIGH:")
+print(high[-5:])
+print("LOW:")
+print(low[-5:])
+
+print("MLP:")
 print(pre1)
+print("RF:")
 print(pre2)
+print("BAYES:")
 print(pre3)
-print(data["inter-diff"][-4:])
+#----------------------------------------------------------
